fac5e415cdf0f694a3ce94cb216fbd81aa29cac6
Karan Pandher <karanpandher@live.ca>
2020-03-09 09:43:46 -0600

    Adding Elements and Vars

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
-// Javascript 
+
+//Adds a new task 
+var taskInput=document.getElementById("new-task");
+//First button 
+var addButton=document.getElementsByTagName("button");
+// For Incomplete tasks 
+var incompleteTaskHolder=document.getElementById("incomplete-tasks");
+// for Completed Tasks 
+var completedTaskHolder=document.getElementById("completed-tasks");
